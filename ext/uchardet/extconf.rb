508639ad5f845bc00e7b5a321bf24026e782b044
dg <dg@syrec.org>
2009-12-20 20:01:46 +0300

    fixed 'missing separator' on Debian/Ubuntu

--- ext/uchardet/extconf.rb
+++ ext/uchardet/extconf.rb
@@ -5,7 +5,8 @@ if icu_config.empty?
   abort "ICU seems to be missing. Try 'port install icu' or 'apt-get install libicu-dev'"
 end
 
-$LIBS << ' ' + `#{icu_config} --ldflags-system --ldflags-libsonly`.strip
+$LIBS << ' ' + `#{icu_config} --ldflags-system`.strip
+$LIBS << ' ' + `#{icu_config} --ldflags-libsonly`.strip
 $LDFLAGS <<  ' ' + `#{icu_config} --ldflags-searchpath`.strip
 
 create_makefile("uchardet")
